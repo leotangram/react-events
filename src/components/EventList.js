15a3670cb9ed7e0ebcba6fc435dc19fdeb1636a6
Leonardo Omaña <leotangram@gmail.com>
2019-06-03 07:49:59 -0500

    Show event info.

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -7,7 +7,9 @@ const EventList = () => {
     <div className='uk-child-width-1-3@m' uk-grid='true'>
       <EventsConsumer>
         {value => {
-          return value.events.map(event => <Event />)
+          return value.events.map(event => (
+            <Event key={event.id} event={event} />
+          ))
         }}
       </EventsConsumer>
     </div>
